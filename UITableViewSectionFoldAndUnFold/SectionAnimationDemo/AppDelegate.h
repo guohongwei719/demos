41fa9433df1c8047ee2bbee964b309922be18096
guohongwei719 <hongweiguo@anjuke.com>
2016-08-24 11:29:31 +0800

    UITableView分区展开与收起

---
+++ UITableViewSectionFoldAndUnFold/SectionAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SectionAnimationDemo
+//
+//  Created by huangyibiao on 16/6/8.
+//  Copyright © 2016年 huangyibiao. All rights reserved.
+//  http://www.huangyibiao.com/ios-tableview-section-animation/
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
