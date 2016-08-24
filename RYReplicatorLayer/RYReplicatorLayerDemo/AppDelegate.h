1fc974fd730cd26714688733d1f0bdd40d2f519c
guohongwei719 <hongweiguo@anjuke.com>
2016-08-24 11:22:31 +0800

    贝塞尔曲线1

---
+++ RYReplicatorLayer/RYReplicatorLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RYReplicatorLayerDemo
+//
+//  Created by billionsfinance-resory on 15/11/7.
+//  Copyright © 2015年 Resory. All rights reserved.
+//  http://www.jianshu.com/p/a927157ac62a
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
